a8255c40a492c7c0cb1af5cbed295d6dc91d7fe3
Nikhil Devasia <nikhildevasia2000@gmail.com>
2023-09-27 14:15:35 +0530

    prediction pipeline is completed

--- src/Gemstone/components/__init__.py
+++ src/Gemstone/components/__init__.py
@@ -1,4 +1,4 @@
 from Gemstone.components.data_ingestion import IngestData
-from Gemstone.components.data_cleaning import DataCleaning
-from Gemstone.components.model_train import ModelTrainer
+from Gemstone.components.data_cleaning import DataCleaning, DataConfig
+from Gemstone.components.model_train import ModelTrainer, ModelTrainerConfig
 from Gemstone.components.model_evaluation import Evaluation
